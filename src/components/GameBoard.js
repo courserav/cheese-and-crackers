a7d471b3e35f3b9abb8c888730f036bcc5b0fafc
courserav <lvsmlit@gmail.com>
2021-10-31 02:51:50 -0400

    adding route files

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -4,12 +4,12 @@ import GameRenderer from './GameRenderer'
 export default class GameBoard extends Component{
     handleClick = (event) => {
         event.preventDefault()
-
+        
     }
     render(){
         return(
-            <div onClick={event => this.handleClick(event)}>
-                <GameRenderer board={this.props.board}/>
+            <div>
+                <GameRenderer onClick={this.handleClick()} board={this.props.board}/>
             </div>
         )
     }
