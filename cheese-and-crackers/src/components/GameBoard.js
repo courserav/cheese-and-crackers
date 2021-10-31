06fcd12f22fdadf7b63a0587924a67add75d80a6
courserav <lvsmlit@gmail.com>
2021-10-31 05:11:04 -0400

    new lvl

--- cheese-and-crackers/src/components/GameBoard.js
+++ cheese-and-crackers/src/components/GameBoard.js
@@ -4,12 +4,12 @@ import GameRenderer from './GameRenderer'
 export default class GameBoard extends Component{
     handleClick = (event) => {
         event.preventDefault()
-        
+
     }
     render(){
         return(
             <div>
-                <GameRenderer onClick={this.handleClick()} board={this.props.board}/>
+                <GameRenderer board={this.props.board}/>
             </div>
         )
     }
--- cheese-and-crackers/src/components/GameRenderer.js
+++ cheese-and-crackers/src/components/GameRenderer.js
@@ -6,9 +6,10 @@ import { Col } from 'react-bootstrap'
 const GameRenderer = ({board}) => {
 
         let renderedBoard = []
+        let spaceCount = 1
 
         for (let i = 0; i < board.length; i++){
-            renderedBoard.push(<Row sm={3}>{board.map(space => <Col>{space}</Col>)}</Row>)
+            renderedBoard.push(<Row sm={3}>{board.map(space => <Col id={spaceCount++}>{space}</Col>)}</Row>)
         }
         return(
             <Container>{renderedBoard}</Container>
